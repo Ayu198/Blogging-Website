d9382fbf1cbc0d2ad7b50f9345ecc53cbfa9fc74
Ayu198 <ayushgupta1918@gmail.com>
2026-06-23 18:31:42 +0530

    More Components

--- Blogging Project/src/components/index.js
+++ Blogging Project/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "/Header/header.jsx"
 import Footer from "/Footer/footer.jsx"
+import Logo from "./Logo"
+import Container from "./container/Container.jsx"
+import LogoutBtn from "./Header/LogoutBtn.jsx"
 
 export {
-    Header ,
-    Footer
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
